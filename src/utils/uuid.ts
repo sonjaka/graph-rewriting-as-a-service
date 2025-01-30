d0754a2ecdb7919822e8d984c6dcd75febe1a900
Sonja Hartl <sonja-karl@web.de>
2025-01-30 23:33:09 +0100

    chore: add uuid utils

---
+++ src/utils/uuid.ts
@@ -0,0 +1,13 @@
+import { v4 as uuid } from 'uuid';
+
+function createNormalizedUuid() {
+	return `${uuid().replaceAll('-', '')}`;
+}
+
+export function createNodeUuid() {
+	return 'n_' + createNormalizedUuid();
+}
+
+export function createEdgeUuid() {
+	return 'e_' + createNormalizedUuid();
+}
