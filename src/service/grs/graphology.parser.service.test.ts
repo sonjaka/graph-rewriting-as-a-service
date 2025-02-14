2e50504f82701a5e45522dd3af1da2061618a843
Sonja Hartl <sonja-karl@web.de>
2025-02-14 23:03:44 +0100

    fix: fixed typing in graphology parser

--- src/service/grs/graphology.parser.service.test.ts
+++ src/service/grs/graphology.parser.service.test.ts
@@ -2,12 +2,15 @@ import { describe, expect, test } from 'vitest';
 import { GraphologyParserService } from './graphology.parser.service';
 
 import sampleGraphData from './testutils/samplegraph.json';
+import { SerializedGraph } from 'graphology-types';
 
 describe('Test graphology parser service', () => {
 	test('Test graphologyParser should add internalId', async () => {
 		const graphParserService = new GraphologyParserService();
 
-		const parsedGraph = graphParserService.parseGraph(sampleGraphData);
+		const parsedGraph = graphParserService.parseGraph(
+			sampleGraphData as SerializedGraph
+		);
 
 		parsedGraph.forEachNode((node, attributes) => {
 			expect(attributes).toHaveProperty('_grs_internalId');
