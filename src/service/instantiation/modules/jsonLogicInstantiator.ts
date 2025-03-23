43efc1fdb5d0a910f43d8823f787a06962770408
Sonja Hartl <sonja-karl@web.de>
2025-03-23 13:52:36 +0100

    chore: updated jsonLogicInstantiator to use JsonPath to resolve string values in instantiatedAttributes

--- src/service/instantiation/modules/jsonLogicInstantiator.ts
+++ src/service/instantiation/modules/jsonLogicInstantiator.ts
@@ -1,31 +1,28 @@
 import JsonLogic from 'json-logic-js';
-import type { AdditionalOperation } from 'json-logic-js';
+import { JSONPath } from 'jsonpath-plus';
+import type { AdditionalOperation, RulesLogic } from 'json-logic-js';
+
 import { IValueInstantiator, IValueInstantiatorOptions } from '../types';
-import { GraphNodeSchema } from '../../../types/node.schema';
-import { GraphEdgeSchema } from '../../../types/edge.schema';
+import { GraphSchema } from '../../../types/graph.schema';
 
-type MatchesMap = Map<string, GraphNodeSchema | GraphEdgeSchema>;
-
-interface JsonLogicRule {
-	if: AdditionalOperation;
-	then: string;
-	result?: boolean;
-	data: unknown;
-}
+type JsonLogicRule = RulesLogic<AdditionalOperation>;
 
 interface JsonLogicInstantiatorOptions extends IValueInstantiatorOptions {
-	rules: JsonLogicRule[]; // Type this correctly
-	matchesMap: MatchesMap;
+	rule: RulesLogic<AdditionalOperation>; // Array of rules to evaluate
+	data?: unknown; // Array of rules to evaluate
+	match: GraphSchema; // Map of matches for resolving placeholders
 }
 
 export enum JsonLogicErrors {
 	'RulesNotProvided' = 'JsonLogic value instantiation: "rules" parameter needs to be provided.',
-	'MatchesNotProvided' = 'JsonLogic value instantiation: "matchesMap" parameter needs to be provided.',
+	'MatchNotProvided' = 'JsonLogic value instantiation: "match" parameter needs to be provided.',
 	'EvaluationFailed' = 'JsonLogic value instantiation: Evaluating jsonLogic rule failed.',
-	'IncompleteRuleConfig' = 'JsonLogic value instantiation: Rule is missing if statement.',
-	'NodePropertyNotFound' = 'JsonLogic value instantiation: Node Property referenced in key not found in matched searchgraph node.',
-	'NodeNotFound' = 'JsonLogic value instantiation:  Node referenced in key not found in searchgraph.',
-	'NodeFormatIncorrect' = 'JsonLogic value instantiation: Node reference incorrect. Should follow the pattern "$.nodeKey.nodePatternName"',
+}
+
+export enum JsonPathErrors {
+	'PathAmbigous' = 'JsonLogic value instantiation: JSON Path result is ambigous.',
+	'PathUndefined' = 'JsonLogic value instantiation: JSON Path result is undefined.',
+	'PathUnresolvable' = 'JSON Path could not be resolved',
 }
 
 export class JsonLogicInstantiator
@@ -37,119 +34,94 @@ export class JsonLogicInstantiator
 		return this._instantiatorKey;
 	}
 
-	public instantiate(args: JsonLogicInstantiatorOptions) {
-		const { rules, matchesMap } = args;
+	public instantiate(
+		args: JsonLogicInstantiatorOptions
+	): string | number | boolean {
+		const { rule, data, match } = args;
 
-		if (!rules) {
+		if (!rule) {
 			throw new Error(JsonLogicErrors.RulesNotProvided);
 		}
 
-		if (!matchesMap) {
-			throw new Error(JsonLogicErrors.MatchesNotProvided);
+		if (!match) {
+			throw new Error(JsonLogicErrors.MatchNotProvided);
 		}
 
-		if (rules) {
-			rules.map((ruleConfig) => {
-				return this.resolveJsonLogicRules(ruleConfig, matchesMap);
-			});
-
-			for (const ruleConfig of rules) {
-				// Return the first positive rule test
-				if (ruleConfig?.result && ruleConfig?.then) {
-					return ruleConfig?.then;
-				}
-			}
+		if (rule) {
+			return this.resolveJsonLogicRule(rule, match, data);
 		}
 
 		return '';
 	}
 
-	private resolveJsonLogicRules(
-		ruleConfig: JsonLogicRule,
-		matchesMap: MatchesMap
-	) {
-		// config includes multiple rules
-		// each rule can contain a
-		// - only a rule - the rules return value is the returned value
-		// - a rule and a data object
-		// - a rule and a then value - the value is the returned value
-		if (ruleConfig?.if) {
-			// replace placeholder values from searchgraph match nodes
-			for (const [operator, jsonLogicRule] of Object.entries(ruleConfig?.if)) {
-				ruleConfig.if[operator] = jsonLogicRule.map(
-					(item: AdditionalOperation | unknown) => {
-						return this.resolveSearchGraphValues(item, matchesMap);
-					}
-				);
-			}
-
-			const data = ruleConfig?.data ?? {};
-			try {
-				ruleConfig['result'] = JsonLogic.apply(ruleConfig.if, data);
-			} catch (err) {
-				// TODO: implement better logging
-				console.log(err);
-				throw new Error(JsonLogicErrors.EvaluationFailed);
-			}
-		} else {
-			throw new Error(JsonLogicErrors.IncompleteRuleConfig);
-		}
-
-		return ruleConfig;
-	}
-
-	private resolveSearchGraphValues(
-		value: AdditionalOperation | unknown,
-		matchesMap: MatchesMap
-	) {
-		if (value && typeof value === 'object') {
-			for (const [operator, values] of Object.entries(value)) {
-				if (operator !== 'var') {
-					// if the operator is "var" this is a data reference which should not be instantiated
-					// else check if search graph value needs to be instantitated
-					const instantiatedValues = values.map((item: unknown) => {
-						return this.resolveSearchGraphValues(item, matchesMap);
-					});
-
-					(value as Record<string, unknown>)[operator] = instantiatedValues;
-				}
-			}
-		}
-
-		if (typeof value === 'string' && value.startsWith('$.')) {
-			// this is a template string that refernces a matched node
-			const segments = value.split('.');
-			// there have to be at three segments
-			// 1. "$."
-			// 2. Node Key in Searchgraph
-			// 3. Node Property in Searchgraph Match
-			if (segments.length === 3) {
-				const node = matchesMap.has(segments[1])
-					? matchesMap.get(segments[1])
-					: null;
-
-				if (node) {
-					if (node.attributes[segments[2]]) {
-						return node.attributes[segments[2]];
-					} else {
-						throw new Error(
-							JsonLogicErrors.NodePropertyNotFound +
-								`, property: ${segments[2]}, key: ${value}`
-						);
-					}
-				} else {
-					throw new Error(
-						JsonLogicErrors.NodeNotFound +
-							`, node: ${segments[1]}, key: ${value}`
+	private resolveJsonLogicRule(
+		rule: RulesLogic<AdditionalOperation>,
+		graph: GraphSchema,
+		data?: unknown
+	): string | number | boolean {
+		if (rule && typeof rule === 'object') {
+			for (const [key, value] of Object.entries(rule)) {
+				if (Array.isArray(value)) {
+					(rule as AdditionalOperation)[key] = value.map((item) =>
+						this.resolveSearchGraphValue(item, graph)
+					);
+				} else if (typeof value === 'string') {
+					(rule as AdditionalOperation)[key] = this.resolveSearchGraphValue(
+						value,
+						graph
 					);
 				}
-			} else {
-				throw new Error(
-					JsonLogicErrors.NodeFormatIncorrect + `, key: ${value}`
-				);
 			}
 		}
 
-		return value;
+		try {
+			// since JsonLogic results can be different data types (e.g. arrays)
+			// we need to turn these into strings if not primary data type
+			const result = JsonLogic.apply(rule, data);
+			if (['string', 'boolean', 'number'].includes(typeof result)) {
+				return result;
+			} else {
+				return String(result);
+			}
+		} catch (err) {
+			// TODO: implement better logging
+			console.log(err);
+			throw new Error(JsonLogicErrors.EvaluationFailed);
+		}
+	}
+
+	private resolveSearchGraphValue(value: JsonLogicRule, graph: GraphSchema) {
+		if (value && typeof value === 'object') {
+			// eslint-disable-next-line prefer-const
+			for (let [key, subValue] of Object.entries(value)) {
+				if (Array.isArray(subValue)) {
+					subValue = subValue.map((item) => {
+						const resolvedItem = this.resolveSearchGraphValue(item, graph);
+						return resolvedItem;
+					});
+				} else if (typeof subValue === 'string') {
+					subValue = this.resolveSearchGraphValue(subValue, graph);
+				}
+				// TODO: fix typing?
+				(value as AdditionalOperation)[key] = subValue;
+			}
+		}
+		if (value && typeof value === 'string' && value.startsWith('$.')) {
+			try {
+				const result = JSONPath({ path: value, json: graph, wrap: false });
+
+				if (result.length > 1) {
+					throw new Error(JsonPathErrors.PathAmbigous);
+				}
+				if (result[0] === undefined) {
+					throw new Error(JsonPathErrors.PathUndefined);
+				}
+				return result.shift();
+			} catch {
+				throw new Error(JsonPathErrors.PathUnresolvable);
+			}
+		} else {
+			return value;
+		}
 	}
 }
