c4fbb5cab3bc2f7b8a522d1d4780d83c4ea16e04
Sonja Hartl <sonja-karl@web.de>
2025-04-04 09:41:03 +0200

    refactor: moved main rewritie algorithm to extra class

--- src/service/grs/graphTransformation.service.ts
+++ src/service/grs/graphTransformation.service.ts
@@ -21,14 +21,13 @@ import {
 import { GraphNodeSchema } from '../../types/node.schema';
 import { GraphEdgeSchema } from '../../types/edge.schema';
 import { RewritingRuleProcessingConfigSchema } from '../../types/run-config.schema';
-import { createEdgeUuid, createNodeUuid } from '../../utils/uuid';
 import { InstantiatorService } from '../instantiation/instantiator.service';
 import { PatternGraphSchema } from '../../types/patterngraph.schema';
-import { PatternNodeSchema } from '../../types/patternnode.schema';
 import { ReplacementGraphSchema } from '../../types/replacementgraph.schema';
 import { ReplacementNodeSchema } from '../../types/replacementnode.schema';
 import { ReplacementEdgeSchema } from '../../types/replacementedge.schema';
 import { getRandomIntBetween } from '../../utils/numbers';
+import { SpoRewriteService } from './spo-rewrite.service';
 
 export type ResultGraphSchema = Omit<GraphSchema, 'nodes' | 'edges'> & {
 	nodes: (Omit<GraphNodeSchema, 'attributes'> & {
@@ -38,17 +37,6 @@ export type ResultGraphSchema = Omit<GraphSchema, 'nodes' | 'edges'> & {
 		attributes: DBGraphEdgeMetadata;
 	})[];
 };
-
-type NodeMatchMap = Map<string, ReplacementNodeSchema | undefined>;
-type EdgeMatchMap = Map<string, ReplacementEdgeSchema | undefined>;
-interface GraphDiffResult {
-	updatedNodes: NodeMatchMap;
-	addedNodes: ReplacementNodeSchema[];
-	removedNodes: PatternNodeSchema[];
-	updatedEdges: EdgeMatchMap;
-	addedEdges: ReplacementEdgeSchema[];
-	removedEdges: GraphEdgeSchema[];
-}
 
 export class GraphTransformationService {
 	private instantiatorService;
@@ -139,6 +127,8 @@ export class GraphTransformationService {
 					replacementGraph
 				);
 
+				this.updateHistory();
+
 				return;
 			}
 
@@ -189,10 +179,7 @@ export class GraphTransformationService {
 						replacementGraph
 					);
 
-					if (this.trackHistory) {
-						const currentHostgraph = await this.exportHostgraph();
-						this.history.push(currentHostgraph);
-					}
+					this.updateHistory();
 				}
 			}
 		}
@@ -218,12 +205,8 @@ export class GraphTransformationService {
 	) {
 		const rhsInstantiated = this.instantiateAttributes(rhs, match);
 
-		const overlapAndDifference = this.computeOverlapAndDifferenceOfLhsAndRhs(
-			lhs,
-			rhsInstantiated
-		);
-
-		await this.replaceMatch(match, overlapAndDifference);
+		const rewriteService = new SpoRewriteService(this.graphService);
+		await rewriteService.performReplacement(match, lhs, rhsInstantiated);
 	}
 
 	public async importHostgraph(
@@ -276,184 +259,6 @@ export class GraphTransformationService {
 		}
 
 		return { options, nodes, edges };
-	}
-
-	/**
-	 * Main Algorithm performing the actual replacing of the pattern match
-	 * 1. Removes all nodes/edges that are in LHS, but NOT part of the RHS
-	 * 2. Updates all nodes/edges that are in LHS and part of the RHS
-	 * 3. Adds all nodes/edges that are part of the RHS but not the LHS
-	 *
-	 * @param occurence The match found in the database for the given lhs pattern
-	 * @param adjustment The difference (added, updated, removed) between the LHS and RHS (gluing interface)
-	 */
-	private async replaceMatch(
-		occurence: DBGraphPatternMatchResult,
-		adjustments: GraphDiffResult
-	) {
-		const preservedNodes: Record<string, string> = {};
-
-		if (Object.entries(occurence.nodes).length) {
-			// Remove all nodes and edges that are not in the replacement graph
-			const removedNodeIds = adjustments.removedNodes.map((node) => {
-				return occurence.nodes[node.key].key;
-			});
-			await this.graphService.deleteNodes(removedNodeIds);
-		}
-
-		if (Object.entries(occurence.edges).length) {
-			const removedEdgesIds = adjustments.removedEdges.map((edge) => {
-				return occurence.edges[edge.key].key;
-			});
-			await this.graphService.deleteEdges(removedEdgesIds);
-		}
-
-		// Update all nodes and edges that are part of both search pattern and replacement graph
-		if (Object.entries(occurence.nodes).length) {
-			for (const [key, rhsNode] of adjustments.updatedNodes) {
-				if (rhsNode) {
-					const oldNode = occurence.nodes[key];
-					const internalId = oldNode.key;
-
-					let options = {};
-					if (rhsNode?.rewriteOptions) {
-						options = rhsNode.rewriteOptions;
-					}
-
-					await this.graphService.updateNode(
-						rhsNode.attributes ?? {},
-						internalId,
-						oldNode.attributes?.type ? [oldNode.attributes?.type] : [],
-						options
-					);
-
-					preservedNodes[key] = internalId;
-				}
-			}
-		}
-
-		if (Object.entries(occurence.edges).length) {
-			for (const [key, rhsEdge] of adjustments.updatedEdges) {
-				if (rhsEdge) {
-					const oldEdge = occurence.edges[key];
-					const internalId = oldEdge.key;
-
-					const sourceInternalId = preservedNodes[rhsEdge.source];
-					const targetInternalId = preservedNodes[rhsEdge.target];
-
-					let options = {};
-					if (rhsEdge?.rewriteOptions) {
-						options = rhsEdge.rewriteOptions;
-					}
-
-					await this.graphService.updateEdge(
-						sourceInternalId,
-						targetInternalId,
-						internalId,
-						rhsEdge.attributes ?? [],
-						options
-					);
-
-					preservedNodes[key] = internalId;
-				}
-			}
-		}
-
-		// Add all new nodes & edges
-		for (const rhsNode of adjustments.addedNodes) {
-			const internalId = createNodeUuid();
-
-			await this.graphService.createNode(rhsNode.attributes ?? {}, internalId);
-
-			preservedNodes[rhsNode.key] = internalId;
-		}
-		for (const rhsEdge of adjustments.addedEdges) {
-			const internalId = createEdgeUuid();
-
-			const sourceInternalId = preservedNodes[rhsEdge.source];
-			const targetInternalId = preservedNodes[rhsEdge.target];
-
-			await this.graphService.createEdge(
-				sourceInternalId,
-				targetInternalId,
-				internalId,
-				rhsEdge.attributes
-			);
-		}
-
-		return;
-	}
-
-	private computeOverlapAndDifferenceOfLhsAndRhs(
-		lhs: PatternGraphSchema,
-		rhs: ReplacementGraphSchema
-	): GraphDiffResult {
-		const updatedNodes: NodeMatchMap = new Map();
-		const removedNodes: PatternNodeSchema[] = [];
-		const addedNodes: ReplacementNodeSchema[] = [];
-
-		const updatedEdges: EdgeMatchMap = new Map();
-		const removedEdges: GraphEdgeSchema[] = [];
-		const addedEdges: ReplacementEdgeSchema[] = [];
-
-		// All nodes in search graph that are also in replacement are "updated"
-		// All nodes in search graph that are not in replacement are "deleted"
-		for (const lhsNode of lhs.nodes) {
-			const rhsNode = rhs.nodes.find((rhsNode) => rhsNode.key === lhsNode.key);
-
-			if (rhsNode) {
-				updatedNodes.set(lhsNode.key, rhsNode);
-			} else {
-				removedNodes.push(lhsNode);
-			}
-		}
-
-		// All nodes that are in replacement but not in search graph are "added".
-		// All search graph nodes should already be part of updated/removed, so if it
-		// can't be found there, it has to be a new/added node
-		for (const rhsNode of rhs.nodes) {
-			if (!updatedNodes.has(rhsNode.key)) {
-				addedNodes.push(rhsNode);
-			}
-		}
-
-		// All edges in search graph that are also in replacement are "updated"
-		// --> An edge is only identical, if both key, source and target match!
-		// --> Nodes cannot be updated and will be deleted and recreated
-		// TODO: Figure out what to do about attributes that are not explicitly specified
-		// All edges in search graph that are not in replacement are "deleted"
-		for (const lhsEdge of lhs.edges) {
-			const rhsEdge = rhs.edges.find(
-				(rhsEdge) =>
-					rhsEdge.key === lhsEdge.key &&
-					rhsEdge.source === lhsEdge.source &&
-					lhsEdge.target === rhsEdge.target
-			);
-
-			if (rhsEdge) {
-				updatedEdges.set(lhsEdge.key, rhsEdge);
-			} else {
-				removedEdges.push(lhsEdge);
-			}
-		}
-
-		// All edges that are in replacement but not in search graph are "added".
-		// All search graph edges should already be part of updated/removed, so if it
-		// can't be found there, it has to be a new/added edge
-		for (const rhsEdge of rhs.edges) {
-			if (!updatedEdges.has(rhsEdge.key)) {
-				addedEdges.push(rhsEdge);
-			}
-		}
-
-		return {
-			updatedNodes,
-			updatedEdges,
-			removedNodes,
-			removedEdges,
-			addedNodes,
-			addedEdges,
-		};
 	}
 
 	private instantiateAttributes(
@@ -521,5 +326,12 @@ export class GraphTransformationService {
 			}
 		}
 		return item;
+	}
+
+	private async updateHistory() {
+		if (this.trackHistory) {
+			const currentHostgraph = await this.exportHostgraph();
+			this.history.push(currentHostgraph);
+		}
 	}
 }
