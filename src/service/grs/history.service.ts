5de5e32971723a4f9f952ff7aefa2a2d0229fcbc
Sonja Hartl <sonja-karl@web.de>
2025-04-15 22:20:03 +0200

    chore: moved history to historyService

---
+++ src/service/grs/history.service.ts
@@ -0,0 +1,26 @@
+import { ResultGraphSchema } from './graphTransformation.service';
+
+export class HistoryService {
+	private history: ResultGraphSchema[] = [];
+	private _trackHistory = false;
+
+	get trackHistory() {
+		return this._trackHistory;
+	}
+
+	set trackHistory(trackHistory: boolean) {
+		this._trackHistory = trackHistory;
+	}
+
+	public addToHistory(graph: ResultGraphSchema): void {
+		this.history.push(graph);
+	}
+
+	public getHistory(): ResultGraphSchema[] {
+		return this.history;
+	}
+
+	public clearHistory(): void {
+		this.history = [];
+	}
+}
