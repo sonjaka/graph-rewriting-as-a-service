af525a3baf881adc6a0157ad725cab3e49d09707
Sonja Hartl <sonja-karl@web.de>
2025-04-17 12:09:10 +0200

    chore: fixed schemas

--- src/types/find-rule.schema.d.ts
+++ src/types/find-rule.schema.d.ts
@@ -6,6 +6,7 @@
  */
 
 export interface GraphFindRuleSchema {
+  key?: string;
   patternGraph: PatternGraphSchema;
   options?: {
     homomorphic?: boolean;
--- src/types/request-find.schema.d.ts
+++ src/types/request-find.schema.d.ts
@@ -11,7 +11,7 @@ export interface GraphFindPatternRequestSchema {
    * @minItems 1
    * @maxItems 1
    */
-  rules: [GraphRewritingRuleSchema];
+  rules: [GraphFindRuleSchema];
 }
 export interface GraphSchema {
   options: {
@@ -62,10 +62,9 @@ export interface GraphEdgeSchema {
   };
   additionalProperties?: never;
 }
-export interface GraphRewritingRuleSchema {
-  key: string;
+export interface GraphFindRuleSchema {
+  key?: string;
   patternGraph: PatternGraphSchema;
-  replacementGraph: ReplacementGraphSchema | ExternalReplacementGraphConfig;
   options?: {
     homomorphic?: boolean;
   };
@@ -97,73 +96,4 @@ export interface NacSchema {
   };
   nodes: PatternNodeSchema[];
   edges: GraphEdgeSchema[];
-}
-export interface ReplacementGraphSchema {
-  options: {
-    /**
-     * Type of the graph: either directed or undirected
-     */
-    type: "directed" | "undirected";
-  };
-  nodes: ReplacementNodeSchema[];
-  edges: ReplacementEdgeSchema[];
-}
-export interface ReplacementNodeSchema {
-  key: string;
-  attributes?: {
-    [k: string]: number | string | boolean | null | GraphInstantiatedAttributeSchema;
-  };
-  rewriteOptions?: {
-    /**
-     * Defines how the attributes are handles during rewrite. 'Modify' mode adds or updates the given attributes. Setting an attribute to null deletes it. 'Replace' mode deletes all attributes of the matched node and then sets the given attributes. 'Delete' mode deletes all attributes and doesn't add any new ones.
-     */
-    attributeReplacementMode?: "modify" | "replace" | "delete";
-  };
-}
-export interface GraphInstantiatedAttributeSchema {
-  type: "randexp" | "jsonLogic" | "faker";
-  args: {
-    [k: string]: unknown;
-  };
-}
-export interface ReplacementEdgeSchema {
-  /**
-   * The edge's ID
-   */
-  key: string;
-  /**
-   * The key of the node at the edge's source
-   */
-  source: string;
-  /**
-   * The key of the node at the edge's target
-   */
-  target: string;
-  /**
-   * The edges attributes & values
-   */
-  attributes: {
-    type?: string;
-    /**
-     * This interface was referenced by `undefined`'s JSON-Schema definition
-     * via the `patternProperty` "^(?!type$).*".
-     */
-    [k: string]: number | string | boolean | GraphInstantiatedAttributeSchema;
-  };
-  rewriteOptions?: {
-    /**
-     * Defines how the attributes are handles during rewrite. 'Modify' mode adds or updates the given attributes (setting an attribute to null deletes it). 'Replace' mode deletes all attributes of the matched node and then sets the given attributes. 'Delete' mode deletes all attributes and doesn't add any new ones.
-     */
-    attributeReplacementMode?: "modify" | "replace" | "delete";
-  };
-}
-export interface ExternalReplacementGraphConfig {
-  type: "externalApi";
-  args?: ExternalApiInstantiationOptions;
-}
-export interface ExternalApiInstantiationOptions {
-  endpoint: string;
-  additionalRequestBodyParameters?: {
-    [k: string]: unknown;
-  };
 }
